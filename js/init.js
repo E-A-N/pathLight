ab05636b988a10b74c946bc89bb14447f7b82e9e
Cyborg_Ean <ean.director@gmail.com>
2018-11-25 18:30:05 -0800

    testing out debugging via query parameters

--- js/init.js
+++ js/init.js
@@ -8,3 +8,13 @@ var w = window.innerWidth * window.devicePixelRatio,
 */
 
 var game = new Phaser.Game(w, h, Phaser.AUTO, 'gameContainer');
+
+//protected and none global!!
+if (config.default.debug.isOn === true){
+    let query = new URLSearchParams(window.location.search);
+    let width =  query.get("dWidth")  || config.init.screenWidth;
+    let height = query.get("dHeight") || config.init.screenHeight;
+
+    game._width = width;
+    game._height = height;
+}
